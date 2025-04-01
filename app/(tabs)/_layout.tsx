27d0981fa11b6c3cddeee924a1ec856afd23b87b
Thanh Dat Vu <thanhdatvu.203@gmail.com>
2025-04-01 14:59:36 -0700

    feat: implement custom bottom tab bar with dynamic TabIcon styling based on focus state

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,91 @@
 import { Tabs } from "expo-router";
+import { ImageBackground, Image, Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 
-const _Layout = () => {
+function TabIcon({ focused, icon, title }: any) {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+      >
+        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Text className="text-secondary text-base font-semibold ml-2">
+          {title}
+        </Text>
+      </ImageBackground>
+    );
+  }
+
   return (
-    // Tab layout that manages navigation inside the (tabs) folder; hides headers for all tab screens
-    <Tabs>
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+    </View>
+  );
+}
+
+export default function TabsLayout() {
+  //The <Tabs> component from expo-router is built on React Navigation's bottom tab navigator,
+  //so it automatically renders the bottom tab bar for you.
+  return (
+    //// Customize the look and feel of the bottom tab bar (rounded, hidden label, styled background)
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0F0D23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0F0D23",
+        },
+      }}
+    >
+      {/*'focused' is a boolean automatically provided by React Navigation to indicate 
+      // if the current tab is active. When a user taps a tab, React Navigation updates 
+      // the active route and re-renders each tabBarIcon, setting 'focused: true' for 
+      // the selected tab and 'false' for the others. This allows us to conditionally 
+      // style the active tab differently from the rest.*/}
+
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
+          title: "index",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.home} title="Home" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.search} title="Search" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="save"
+        options={{
+          title: "Save",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.save} title="Save" />
+          ),
         }}
       />
       <Tabs.Screen
@@ -16,24 +93,11 @@ const _Layout = () => {
         options={{
           title: "Profile",
           headerShown: false,
-        }}
-      />
-      <Tabs.Screen
-        name="save"
-        options={{
-          title: "Savee",
-          headerShown: false,
-        }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          title: "Searchh",
-          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.person} title="Profile" />
+          ),
         }}
       />
     </Tabs>
   );
-};
-
-export default _Layout;
+}
