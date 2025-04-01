460618921e0709cb0639feda8d027df9846e9e91
Thanh Dat Vu <thanhdatvu.203@gmail.com>
2025-04-01 13:48:10 -0700

    update css and layout, use <stack> to hides headers for both tabs and movie detail screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,22 @@
 import { Stack } from "expo-router";
+import "./globals.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    // Root stack layout controlling screen navigation; hides headers for both tabs and movie detail screens
+    <Stack>
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="movies/[id]"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
 }
