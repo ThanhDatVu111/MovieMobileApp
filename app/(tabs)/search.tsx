6b3b9984a664d4b0b5596f69d5dd38578688fc95
Thanh Dat Vu <thanhdatvu.203@gmail.com>
2025-04-02 20:23:06 -0700

    feat: implement debounced movie search with dynamic results and loading state

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,14 +1,124 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { useState, useEffect } from "react";
+import { View, Text, ActivityIndicator, FlatList, Image } from "react-native";
+
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+
+import useFetch from "@/services/usefetch";
+import { fetchMovies } from "@/services/api";
+//import { updateSearchCount } from "@/services/appwrite";
+
+import SearchBar from "@/components/SearchBar";
+import MovieCard from "@/components/MovieCard";
 
 const Search = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const {
+    data: movies = [],
+    loading,
+    error,
+    refetch: loadMovies,
+    reset,
+  } = useFetch(() => fetchMovies({ query: searchQuery }), false);
+  // The useFetch hook is called with fetchMovies and searchQuery as arguments.
+
+  // handleSearch is called whenever the user types in the search bar.
+  // setSearchQuery updates the searchQuery state with the new text.
+  const handleSearch = (text: string) => {
+    setSearchQuery(text);
+  };
+
+  // useEffect is call when searchQuery changes.
+  // If there is a search query, it waits for 500ms after the user stops typing before calling loadMovies to fetch the movies.
+  // If the search bar is empty, it clears out the results by calling reset()
+  useEffect(() => {
+    const timeoutId = setTimeout(async () => {
+      // ⏱ Code to run after delay 500ms
+      if (searchQuery.trim()) {
+        await loadMovies();
+      } else {
+        reset();
+      }
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
+  }, [searchQuery]);
+
   return (
-    <View className="flex-1 items-center justify-center bg-primary">
-      <Text className="text-5xl text-white">I'm searching right now</Text>
+    <View className="flex-1 bg-primary">
+      {/*The background image is set to cover the entire screen and is positioned behind all other elements.*/}
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
+      {/* set the app logo */}
+      <View className="w-full flex-row justify-center mt-20 items-center">
+        <Image source={icons.logo} className="w-12 h-10" />
+      </View>
+
+      {/* The FlatList component is used to render a list of movies. */}
+      <FlatList
+        className="px-5"
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "flex-start",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListHeaderComponent={
+          <>
+            <View className="my-5">
+              <SearchBar
+                placeholder="Search for a movie"
+                value={searchQuery}
+                onChangeText={handleSearch} // This function is called whenever the user types in the search bar
+              />
+            </View>
+
+            {loading && (
+              <ActivityIndicator
+                size="large"
+                color="#0000ff"
+                className="my-3"
+              />
+            )}
+
+            {error && (
+              <Text className="text-red-500 px-5 my-3">
+                Error: {error.message}
+              </Text>
+            )}
+
+            {!loading &&
+              !error &&
+              searchQuery.trim() &&
+              movies?.length! > 0 && (
+                <Text className="text-xl text-white font-bold">
+                  Search Results for<Text className="text-accent">{searchQuery}</Text>
+                </Text>
+              )}
+          </>
+        }
+        ListEmptyComponent={
+          !loading && !error ? (
+            <View className="mt-10 px-5">
+              <Text className="text-center text-gray-500">
+                {searchQuery.trim()
+                  ? "What the heck are you searching for mf?"
+                  : "Please search for a movie mf"}
+              </Text>
+            </View>
+          ) : null
+        }
+      />
     </View>
   );
 };
 
 export default Search;
-
-const styles = StyleSheet.create({});
